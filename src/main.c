bf199fe0565d37ee9f841f6ee2142288302f3d11
Thoshio Onuki Neto <thoshio@terra.com.br>
2026-06-12 12:33:37 -0300

    Parte 3  v0

--- src/main.c
+++ src/main.c
@@ -1,27 +1,29 @@
 #include <zephyr/kernel.h>
 
 // Semáforo para sincronização
-K_SEM_DEFINE(vitrine_acessivel, 1, 1);  // Padeiro pode começar
+K_SEM_DEFINE(vitrine_acessivel, 0, 1);  //não há pão na vitrine
 
 // Contador para simular diferentes tipos de pão
-static volatile int saldo_vitrine = 0;
+static volatile int saldo_vitrine = 0; //não há pão na vitrine
 
 int padeiro_thread(void)
 {
-    while (1) {        
-        // Espera permissão para acessar a vitrine
-        k_sem_take(&vitrine_acessivel, K_FOREVER);
-
+    while (1) {
         // Simula tempo para coloocar o pão na vitrine
         k_msleep(1000);  // 1 segundo
         
-        // Coloca pão na vitrine
-        saldo_vitrine += 1;
-        
-        printk("\nPADEIRO: Pao pronto\nVitrine com %d pao(es)\n", saldo_vitrine);
+        // Coloca pão na vitrine se a vitrine tiver espaço
+        if (saldo_vitrine < 10) {
+            saldo_vitrine += 1; // Produz pao
+            printk("\nPADEIRO: Pao pronto\nVitrine com %d pao(es)\n", saldo_vitrine);
+        }
 
-        // vitrine está acessível de novo
-        k_sem_give(&vitrine_acessivel);
+        if (saldo_vitrine > 0){
+            // vitrine está acessível de novo quando tem pão
+            k_sem_give(&vitrine_acessivel);
+        } else {
+            k_sem_take(&vitrine_acessivel, K_FOREVER);
+        }
     }
 }
 
@@ -38,14 +40,11 @@ void cliente_thread(void)
         saldo_vitrine -= 1;
         
         printk("\nCLIENTE: Peguei pao\nVitrine com %d pao(es)\n", saldo_vitrine);
-
-        // vitrine está acessível de novo
-        k_sem_give(&vitrine_acessivel);
     }
 }
 
 // Definir as threads
-K_THREAD_DEFINE(padeiro_tid, 512, padeiro_thread, NULL, NULL, NULL, 6, 0, 0);
+K_THREAD_DEFINE(padeiro_tid, 512, padeiro_thread, NULL, NULL, NULL, 7, 0, 0);
 K_THREAD_DEFINE(cliente_tid, 512, cliente_thread, NULL, NULL, NULL, 7, 0, 0);
 
 int main(void)
