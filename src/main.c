4b00718093fa35dd58b963a557de03d7371a41d7
Thoshio Onuki Neto <thoshio@terra.com.br>
2026-06-12 10:06:01 -0300

    criando base do projeto

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <zephyr/kernel.h>             // Funções básicas do Zephyr (ex: k_msleep, k_thread, etc.)
+#include <zephyr/device.h>             // API para obter e utilizar dispositivos do sistema
+
+
+int main(void)
+{
+       while (1) {
+
+    }
+    return 0;
+}
