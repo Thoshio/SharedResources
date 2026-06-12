4bd5b08cae4d97b3aca59ac13e6f5f4f810d2f05
Thoshio Onuki Neto <thoshio@terra.com.br>
2026-06-12 11:47:46 -0300

    Parte 3 Funcionando

--- src/main.c
+++ src/main.c
@@ -1,36 +1,51 @@
 #include <zephyr/kernel.h>
 
+// Semáforo para sincronização
+K_SEM_DEFINE(vitrine_acessivel, 1, 1);  // Padeiro pode começar
+
 // Contador para simular diferentes tipos de pão
 static volatile int saldo_vitrine = 0;
 
 int padeiro_thread(void)
 {
     while (1) {        
-        // Simula tempo para fazer o pão
-        k_msleep(1000);  // 1 segundo para fazer pão
+        // Espera permissão para acessar a vitrine
+        k_sem_take(&vitrine_acessivel, K_FOREVER);
+
+        // Simula tempo para coloocar o pão na vitrine
+        k_msleep(1000);  // 1 segundo
         
-        // Faz o pão e coloca na vitrine
+        // Coloca pão na vitrine
         saldo_vitrine += 1;
         
         printk("\nPADEIRO: Pao pronto\nVitrine com %d pao(es)\n", saldo_vitrine);
+
+        // vitrine está acessível de novo
+        k_sem_give(&vitrine_acessivel);
     }
 }
 
 void cliente_thread(void)
 {
     while (1) {
-         // Simula tempo para retirar o pão
+        // Espera permissão para acessar a vitrine
+        k_sem_take(&vitrine_acessivel, K_FOREVER);
+
+        // Simula tempo para retirar o pão
         k_msleep(1500);  // 1.5 segundos
 
         // Pega o pão do buffer
         saldo_vitrine -= 1;
         
         printk("\nCLIENTE: Peguei pao\nVitrine com %d pao(es)\n", saldo_vitrine);
+
+        // vitrine está acessível de novo
+        k_sem_give(&vitrine_acessivel);
     }
 }
 
 // Definir as threads
-K_THREAD_DEFINE(padeiro_tid, 512, padeiro_thread, NULL, NULL, NULL, 7, 0, 0);
+K_THREAD_DEFINE(padeiro_tid, 512, padeiro_thread, NULL, NULL, NULL, 6, 0, 0);
 K_THREAD_DEFINE(cliente_tid, 512, cliente_thread, NULL, NULL, NULL, 7, 0, 0);
 
 int main(void)
